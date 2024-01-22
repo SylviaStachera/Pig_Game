6584d6b087be74f750bc3350df9aaec78b0965d8
Sylwia Stachera <113374423+SylviaStachera@users.noreply.github.com>
2024-01-22 20:17:39 +0100

    Added switching game player.

--- src/js/script.js
+++ src/js/script.js
@@ -5,11 +5,14 @@ import img4 from "url:../img/dice-4.png";
 import img5 from "url:../img/dice-5.png";
 import img6 from "url:../img/dice-6.png";
 
+// https://pig-game-v2.netlify.app
 const diceImages = [img1, img2, img3, img4, img5, img6];
-let currentScore;
+let currentScore, activePlayer;
 const score = [0, 0];
 
 const diceImg = document.querySelector(".dice");
+const player01 = document.querySelector(".player--0");
+const player02 = document.querySelector(".player--1");
 const current01 = document.querySelector("#current--0");
 const current02 = document.querySelector("#current--1");
 const score01 = document.querySelector("#score--0");
@@ -19,30 +22,55 @@ const btnHold = document.querySelector(".btn--hold");
 
 const init = function () {
 	currentScore = 0;
+	activePlayer = 0;
 
 	score01.textContent = 0;
+	score02.textContent = 0;
 	current01.textContent = 0;
+	current02.textContent = 0;
+
+	diceImg.classList.add("hidden");
+	player01.classList.add("player--active");
+	player02.classList.remove("player--active");
 };
 init();
 
+const changePlayer = function () {
+	document.querySelector(`#current--${activePlayer}`).textContent = 0;
+	activePlayer = activePlayer === 0 ? 1 : 0;
+	currentScore = 0;
+	player01.classList.toggle("player--active");
+	player02.classList.toggle("player--active");
+};
+
 btnRoll.addEventListener("click", function () {
 	const dice = Math.trunc(Math.random() * 6 + 1);
+
+	diceImg.classList.remove("hidden");
 
 	let srcDice = diceImages[dice - 1];
 	diceImg.setAttribute("src", `${srcDice}`);
 	if (dice !== 1) {
 		currentScore += dice;
-		current01.textContent = currentScore;
+		document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
 	} else {
-		currentScore = 0;
-		current01.textContent = 0;
-		console.log("STOP");
+		changePlayer();
 	}
 });
 
 btnHold.addEventListener("click", function () {
-	score[0] += Number(current01.textContent);
-	currentScore = 0;
-	current01.textContent = 0;
-	score01.textContent = score[0];
+	score[activePlayer] += Number(document.querySelector(`#current--${activePlayer}`).textContent);
+
+	document.querySelector(`#score--${activePlayer}`).textContent = score[activePlayer];
+
+	if (score[activePlayer] >= 10) {
+		document.querySelector(`.player--${activePlayer}`).classList.add("player--winner");
+		document.querySelector(`.player--${activePlayer}`).classList.remove("player--active");
+		diceImg.classList.add("hidden");
+		btnHold.disabled = true;
+		btnRoll.disabled = true;
+	}
+
+	changePlayer();
+	console.log(score);
 });
