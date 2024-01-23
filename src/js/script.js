0f15e6f66f1027819da039fb9c356c607de797f4
Sylwia Stachera <113374423+SylviaStachera@users.noreply.github.com>
2024-01-23 20:36:59 +0100

    Code refactor

--- src/js/script.js
+++ src/js/script.js
@@ -5,18 +5,15 @@ import img4 from "url:../img/dice-4.png";
 import img5 from "url:../img/dice-5.png";
 import img6 from "url:../img/dice-6.png";
 
-// https://pig-game-v2.netlify.app
 const diceImages = [img1, img2, img3, img4, img5, img6];
 let currentScore, activePlayer;
-const score = [0, 0];
+const maxScore = 50;
+const scores = [0, 0];
 
 const diceImg = document.querySelector(".dice");
-const player01 = document.querySelector(".player--0");
-const player02 = document.querySelector(".player--1");
-const current01 = document.querySelector("#current--0");
-const current02 = document.querySelector("#current--1");
-const score01 = document.querySelector("#score--0");
-const score02 = document.querySelector("#score--1");
+const players = document.querySelectorAll(".player");
+const currentScores = document.querySelectorAll(".current-score");
+const totalScore = document.querySelectorAll(".score");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 const btnNew = document.querySelector(".btn--new");
@@ -25,59 +22,61 @@ const init = function () {
 	currentScore = 0;
 	activePlayer = 0;
 
-	score01.textContent = 0;
-	score02.textContent = 0;
-	current01.textContent = 0;
-	current02.textContent = 0;
-
 	diceImg.classList.add("hidden");
-	player01.classList.add("player--active");
-	player02.classList.remove("player--active");
-	player01.classList.remove("player--winner");
-	player02.classList.remove("player--winner");
+
+	currentScores.forEach((score) => (score.textContent = 0));
+	totalScore.forEach((score) => (score.textContent = 0));
+
+	players.forEach((player, index) => {
+		player.classList.toggle("player--active", index === 0);
+		player.classList.remove("player--winner");
+	});
 
 	btnHold.disabled = false;
 	btnRoll.disabled = false;
 };
-init();
 
 const changePlayer = function () {
-	document.querySelector(`#current--${activePlayer}`).textContent = 0;
+	currentScores[activePlayer].textContent = 0;
 	activePlayer = activePlayer === 0 ? 1 : 0;
 	currentScore = 0;
-	player01.classList.toggle("player--active");
-	player02.classList.toggle("player--active");
+	players.forEach((player) => player.classList.toggle("player--active"));
 };
 
 btnRoll.addEventListener("click", function () {
 	const dice = Math.trunc(Math.random() * 6 + 1);
-
 	diceImg.classList.remove("hidden");
 
-	let srcDice = diceImages[dice - 1];
-	diceImg.setAttribute("src", `${srcDice}`);
+	diceImg.setAttribute("src", diceImages[dice - 1]);
+
 	if (dice !== 1) {
 		currentScore += dice;
-		document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
+		currentScores[activePlayer].textContent = currentScore;
 	} else {
 		changePlayer();
 	}
 });
 
 btnHold.addEventListener("click", function () {
-	score[activePlayer] += Number(document.querySelector(`#current--${activePlayer}`).textContent);
+	scores[activePlayer] += currentScore;
 
-	document.querySelector(`#score--${activePlayer}`).textContent = score[activePlayer];
+	totalScore[activePlayer].textContent = scores[activePlayer];
 
-	if (score[activePlayer] >= 50) {
-		document.querySelector(`.player--${activePlayer}`).classList.add("player--winner");
-		document.querySelector(`.player--${activePlayer}`).classList.remove("player--active");
+	if (scores[activePlayer] >= maxScore) {
+		players[activePlayer].classList.add("player--winner");
+		players[activePlayer].classList.remove("player--active");
+
 		diceImg.classList.add("hidden");
+
 		btnHold.disabled = true;
 		btnRoll.disabled = true;
+
+		alert(`Player ${activePlayer + 1} wins!`);
 	} else {
 		changePlayer();
 	}
 });
 
 btnNew.addEventListener("click", init);
+
+init();
